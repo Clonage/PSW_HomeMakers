dc15e5bb71794464873642c884f1de8976adc2e4
Clonage <mitchel.mauricio@gmail.com>
2015-11-06 04:32:36 +0000

    2nd

---
+++ PSW_HomeMakers/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PSW_HomeMakers.Startup))]
+namespace PSW_HomeMakers
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
